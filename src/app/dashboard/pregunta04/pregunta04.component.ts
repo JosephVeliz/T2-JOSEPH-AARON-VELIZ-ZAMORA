4fff438c1479ae902288ca93022edf2d15f8496d
JosephVeliz <aronveliz6@outlook.com>
2024-06-22 23:56:35 -0500

    pregunta04 100%

--- src/app/dashboard/pregunta04/pregunta04.component.ts
+++ src/app/dashboard/pregunta04/pregunta04.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../angular-material/material/material.module';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pregunta04',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule, FormsModule, CommonModule],
   templateUrl: './pregunta04.component.html',
   styleUrl: './pregunta04.component.css'
 })
 export class Pregunta04Component {
+  precioUnitario: number = 0;
+  cantidad: number = 0;
+  totalPagar: number = 0;
+  mensajeDescuento: string = '';
 
+  calcularTotalPagar() {
+    this.totalPagar = this.precioUnitario * this.cantidad;
+
+    if (this.totalPagar > 200) {
+      this.totalPagar *= 0.8; 
+      this.mensajeDescuento = 'Se aplica un descuento del 20%';
+    } else {
+      this.mensajeDescuento = 'No se aplica descuento';
+    }
+  }
 }
